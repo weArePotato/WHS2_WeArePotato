876894a57e52597dbeb7aae60f7264307742ec8f
hibrian827 <hibrian827@gmail.com>
2024-06-14 13:00:53 +0900

    minor changes

--- file_counter.py
+++ file_counter.py
@@ -1,21 +1,21 @@
 import os
 
-npm_dir = './data/benign/npm'
+npm_dir = './data/npm/benign'
 
 js_file_count = sum([len([file for file in files if file.endswith('.js')]) for r, d, files in os.walk(npm_dir)])
 print(f'Total number of benign .js files: {js_file_count}')
 
-npm_dir = './data/malicious/npm'
+npm_dir = './data/npm/malicious'
 
 js_file_count = sum([len([file for file in files if file.endswith('.js')]) for r, d, files in os.walk(npm_dir)])
 print(f'Total number of malicious .js files: {js_file_count}')
 
-pypi_dir = './data/benign/pypi'
+pypi_dir = './data/pypi/benign'
 
 py_file_count = sum([len([file for file in files if file.endswith('.py')]) for r, d, files in os.walk(pypi_dir)])
 print(f'Total number of benign .py files: {py_file_count}')
 
-pypi_dir = './data/malicious/pypi'
+pypi_dir = './data/pypi/malicious'
 
 py_file_count = sum([len([file for file in files if file.endswith('.py')]) for r, d, files in os.walk(pypi_dir)])
 print(f'Total number of malicious .py files: {py_file_count}')
